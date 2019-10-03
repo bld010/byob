c9707156c50a5b503a3feb0272674b86257e88c9
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-03 10:13:02 -0600

    Add server configuration to work knex

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', 3000);
 app.use(express.json());
 
