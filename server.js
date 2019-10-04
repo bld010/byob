68bbef6b229585a62c3bde1579aef29639dc2e7d
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-04 12:49:25 -0600

    Complete annotation on server.js

--- server.js
+++ server.js
@@ -119,41 +119,68 @@ app.get('/api/v1/states/:id', (request, response) => {
     })
 })
 
+
+// set up endpoint for get all senators
 app.get('/api/v1/senators', (request, response) =>  {
+
+  // get the senators table from the database
   database('senators').select()
     .then((senators) => {
+      // check to see if senators array is empty or not
       if (senators.length) {
+        // if the senators array isn't empty, return
+        // 200 status code and senators array in response
         response.status(200).json(senators)
       } else {
+        // otherwise, send 404, no senators found error 
+        // in response
         response.status(404).json({
           error: `No senators found.`
         })
       }
     })
+    // if a server error is thrown, send a 500 status
+    // code and send error in the response
     .catch(error => {
       response.status(500).json({ error })
     })
 })
 
+// endpoint for get specific senator by id
 app.get('/api/v1/senators/:id', (request, response) => {
+  // query the senators table in the database to match the :id
+  // with the id in the database. parseInt'ed because the :id
+  // is a string, and the database id is a number
   database('senators').where('id', parseInt(request.params.id)).select()
     .then(senators => {
+      // check the length of the result of the query
       if (senators.length) {
+        // query isn't empty, so send status 200 and send
+        // array containing matching senator
         response.status(200).json(senators);
       } else {
+        // query is empty, so return 404 status and 
+        // error message
         response.status(404).json({
           error: `Could not find senator with id ${request.params.id}.`
         })
       }
     })
+    // if server error is thrown, send 500 status and 
+    // error message
     .catch(error => {
       response.status(500).json({ error })
     })
 })
 
+// post new senator endpoint
 app.post('/api/v1/senators', (request, response) => {
+  // set variable senator assigned to request body
   let senator = request.body;
 
+
+    // describe required parameters and iterate through
+    // parameters
   for (let requiredParameter of [
     'name', 
     'title',
@@ -163,7 +190,7 @@ app.post('/api/v1/senators', (request, response) => {
     'party',
     'url'
     ]) {
-
+      // error to inform user how to fix their request
       let errorText = `
         Expected format: { 
           name: <String>, 
@@ -174,32 +201,51 @@ app.post('/api/v1/senators', (request, response) => {
           party: <String>, 
           url: <String> }
           You're missing a ${requiredParameter} property.`
-
+          // send error message if a required parameter
+          // is missing from the response body, with a status
+          // code of 422
       if (!senator[requiredParameter]) {
         return response.status(422)
         .json({ error: errorText });
       }
   }
 
+  // if required parameters are present, 
+  // add new senator with .insert()
+  // the second argument returns the unique id 
+  // from the newly-created senator row in 
+  // the database
   database('senators').insert(senator, 'id')
+    // send response with 201 status 
+    // and send the id of the newly-created senator
     .then(senatorId => {
       response.status(201).json({ id: senatorId[0]})
     })
+    // if an error is thrown, return a 500 status
+    // code and error object
     .catch(error => {
       response.status(500).json({ error })
     })
 })
 
+// delete senator by id endpoint
 app.delete('/api/v1/senators/:id', (request, response) => {
-
+  // query senators table in database for matching id from request url
+  // :id must be parsed because database ids are numbers, while the url
+  // parameter is a string
+  // .del() resolves a promise when successful
   database('senators').where('id', parseInt(request.params.id)).select().del()
-    .then(success => {
+  .then(success => {
+    // if the delete returns return a response, send a 204 status code
       if (success) {
-        response.status(204).json({ success })
+        response.status(204)
       } else {
+        // otherwise, send 404 and error message
         response.status(404).send(`Delete unsuccessful. Make sure the senator id is correct in your request url.`)
       }
     })
+    // if server throws an error, return a 500 status 
+    // code and error object
     .catch(error => {
       response.status(500).json({ error })
     })
