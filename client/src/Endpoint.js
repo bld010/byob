363b1fdab58420cd80eaf55e14eef5dd435ce005
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-06 08:29:50 -0600

    Display getAllStates info dynamically

--- client/src/Endpoint.js
+++ client/src/Endpoint.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Endpoint.css'
+import { getAllStates, getSpecificState } from './endpointDescriptions';
+import * as parse from '../node_modules/html-react-parser';
 
 export default class Endpoint extends Component {
 
@@ -8,14 +10,27 @@ export default class Endpoint extends Component {
     this.state = {
       endpoint_name: this.props.endpoint_name,
       user_request_data: null,
+      endpont_info: null
     }
 
   }
   
+  setEndpointInfo() {
+    switch (this.state.endpoint_name) {
+      case "GET all states":
+        return getAllStates;
+
+      // case "GET specific state":
+
+      //   return getSpecificState;
+      default: return null
+    }
+  }
+
 
   componentDidMount() {
 
-  
+    this.setState({ endpoint_info: this.setEndpointInfo() })
   }
 
   render() {
@@ -24,15 +39,18 @@ export default class Endpoint extends Component {
     return (
       <section className="Endpoint">
         <h2>{this.state.endpoint_name}</h2>
-        <p>Description</p>
+        <p>{this.state.endpoint_info && this.state.endpoint_info.description}</p>
         <div>
-          <h4>Parameters Table</h4>
+          <h4>Parameters</h4>
+          {this.state.endpoint_info && parse(this.state.endpoint_info.parameters_table)}
           <p>Example Response</p>
+          {this.state.endpoint_info && parse(this.state.endpoint_info.response_table)}
         </div>
         <div>
           <h3>Where we will display mock fetch? Fetch call component</h3>
           <button>Click here to test endpoint</button> 
           <div>Show results here based on state</div>
+          
         </div>
 
       </section>
--- client/src/endpointDescriptions.js
+++ client/src/endpointDescriptions.js
@@ -1,27 +1,83 @@
+
+const getAllStatesParametersTable = `<table>
+<thead>
+<tr>
+<th>Name</th>
+<th>Type</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td><code>id</code></td>
+<td><code>number</code></td>
+<td>Unique id associated with each state</td>
+</tr>
+<tr>
+<td><code>abbreviation</code></td>
+<td><code>string</code></td>
+<td>Two-letter, capitalized abbreviation for a state or territory</td>
+</tr>
+<tr>
+<td><code>name</code></td>
+<td><code>string</code></td>
+<td>Full name of state or territory</td>
+</tr>
+</tbody>
+</table>`
+
+
+const getAllStatesResponseTable = `
+<table>
+<thead>
+<tr>
+<th>Status</th>
+<th>Response</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>200</td>
+<td>Array of state objects</td>
+</tr>
+</tbody>
+</table>`
+
+const getAllStatesExampleResponse = [
+   {
+        "id": 169,
+        "name": "Louisiana",
+        "abbreviation": "LA",
+        "created_at": "2019-10-03T01:12:17.283Z",
+        "updated_at": "2019-10-03T01:12:17.283Z"
+    },
+    {
+        "id": 171,
+        "name": "North Carolina",
+        "abbreviation": "NC",
+        "created_at": "2019-10-03T01:12:17.289Z",
+        "updated_at": "2019-10-03T01:12:17.289Z"
+    }
+]
+
 export const getAllStates = {
   fetchCall: {
     options: {},
-    request_url: null
+    request_url: '/api/v1/states'
     },
   description: 'Get all states in API',
   endpoint: 'GET /api/v1/states',
-  parameters: statesTable
+  parameters_table: getAllStatesParametersTable,
+  example_response: getAllStatesExampleResponse,
+  response_table: getAllStatesResponseTable
   
 }
 
-const getAllStatesParametersTable = `
-    <table>
-      <tr>
-        <th> Name </th>
-        <th> Type </th>
-        <th> Description </th>
-      </tr>
+// export const getSpecificState = {
+//   fetchCall: {
+//     options: {},
+//     request_url: '/api/v1/states/'
+//   }
+// }
 
-      <tr>
-      </tr>
 
-      <tr>
-
-      </tr>
-    </table>
-    `
