89b3f096b54bba6e645525777b1c99ee95ebab03
Brianna DelValle <briannadelvalle@gmail.com>
2019-10-06 07:16:31 -0600

    Create fetchData method in App

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    states: null,
+    senators: null,
+    house_members: null
+  }
+
+  componentDidMount() {
+    this.fetchData()
+      .catch(err => console.log(err));
+  }
+
+  fetchData = async () => {
+    let urls = ['/api/v1/states', 'api/v1/senators']
+
+    let fetchPromises = urls.map(url => fetch(url))
+
+    Promise.all(fetchPromises)
+      .then(res => Promise.all(res.map(res => res.json())))
+      .then(data => this.setState({ states: data[0], senators: data[1] }))
+      .catch(error => console.log({ error }))
+  }
+
+  render() {
+    return(
+      <>
+    
+      {this.state.states && <p>{this.state.states.length}</p>}
+      <p> Test </p>
+      </>
+
+    )
+  }
 }
 
 export default App;
